facbe7dfa685cc9c2c3784054f72bd643f90b7f7
Gustavo <gustavo.ferraz.coelho@escola.pr.gov.br>
2025-10-17 11:09:50 -0300

    update

---
+++ app.py
@@ -0,0 +1,32 @@
+ players = {
+    '1': {
+        'nome': 'Nilson',
+        'idade': 24,
+        'ryos': 320.50,
+        'classe': 'Mago',
+        'familia': 'Loki',
+        'habilidades': {
+            'ataque': 10,
+            'defesa': 5,
+            'vida': 10
+        },
+        'nivel': 1,
+        'xp': 0,
+        'equipamentos': [0]
+    },
+    '2': {
+        'nome': 'Robison',
+        'idade': 15,
+        'ryos': 320.50,
+        'classe': 'Mago',
+        'familia': 'Mongólis',
+        'habilidades': {
+            'ataque': 23,
+            'defesa': 1,
+            'vida': 1
+        },
+        'nivel': 1,
+        'xp': 0,
+        'equipamentos': [0]
+    }
+}
